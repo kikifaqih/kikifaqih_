191fe49dfca40345ed1b555f212d5160fd5bf53b
kikifaqih <111949833+kikifaqih@users.noreply.github.com>
2023-07-02 01:59:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+    //instagram
+        var ig = document.getElementById("instagram");
+        function handleClick1() {
+            var targetURL1 = "https://www.instagram.com/kikifaqih_/";
+            window.open(targetURL1, "_blank");
+        }
+        ig.addEventListener("click", handleClick1);
+
+    //tiktok
+        var tktok = document.getElementById("tiktok");
+        function handleClick2() {
+            var targetURL2 = "https://www.tiktok.com/@kikifaqih38";
+            window.open(targetURL2, "_blank");
+        }
+        tktok.addEventListener("click", handleClick2);
+    
+    //youtube
+        var yt = document.getElementById("youtube");
+        function handleClick3() {
+            var targetURL3 = "https://www.youtube.com/channel/UCU0UUL5THf59Pe9H9UyUmUg";
+            window.open(targetURL3, "_blank");
+        }
+        yt.addEventListener("click", handleClick3);
+    
+    //spotify
+        var sptfy = document.getElementById("spotify");
+        function handleClick4() {
+            var targetURL4 = "http://bit.ly/kikiiiiiiii";
+            window.open(targetURL4, "_blank");
+        }
+        sptfy.addEventListener("click", handleClick4);
